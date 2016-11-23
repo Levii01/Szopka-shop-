86a329cb9959ee129356fbebcdd0fddc9b18bdc7
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-23 14:04:35 +0100

    fix side nav and product card... need more refactor but it work's c:

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return [{
+      name: 'hello <3'
+    }, {
+      name: 'hello <3'
+    }, {
+      name: 'hello <3'
+    }, {
+      name: 'hello <3'
+    }, {
+      name: 'hello <3'
+    }];
+  }
 });
